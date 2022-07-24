77d058ee9b474284d1bee27269937e83a9dca835
yudur <duarteyudi218@gmail.com>
2022-07-24 15:44:32 -0300

    Assertions

---
+++ calculator.py
@@ -0,0 +1,4 @@
+def soma(x, y):
+    assert isinstance(x, (int, float)), '(x) precisa ser um int ou um float'
+    assert isinstance(y, (int, float)), '(y) precisa ser um int ou um float'
+    return x + y
---
+++ main.py
@@ -0,0 +1,12 @@
+from calculator import soma
+
+# print(soma(10, 50))
+# print(soma(-10, 50))
+# print(soma(1.5, 5.5))
+# print(soma(15, 15))
+
+try:
+    print(soma('14', 12))
+except AssertionError as error:
+    print('conta inválida')
+    print(error)
